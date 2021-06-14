792fd36a270864713cdf0afeacc378442357a79e
Matt <Matthew.White02@gmail.com>
2021-06-14 22:42:29 +0100

    Toggle between content sections

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
 (function() {
+	Array.from(document.getElementsByTagName("nav")[0].children).forEach(e => {
+		e.addEventListener("click", showSectionFromLinkElement);
+	});
 
 })();
+
+	document.getElementsByTagName("nav")[0].firstElementChild.click();
+
+function showSectionFromLinkElement(event) {
+	var name = event.target.textContent.toLowerCase().replace(" ", "-");
+	Array.from(document.getElementsByClassName('content')[0].children).forEach(e => {
+		e.classList.add("hide");
+	});
+	document.getElementById(name).classList.remove("hide");
+}
